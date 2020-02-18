61dbab0fa9605e6613503c50592faf639ef81f0b
Wojciech Urbański <wojciech.urbanski@codilime.com>
2020-02-18 13:30:23 +0100

    Restored debug from mian.go

--- main.go
+++ main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
 	"log"
+	"os"
 
 	"cl-gitlab.intra.codilime.com/spinops/floodgate/cmd/cli"
+	"cl-gitlab.intra.codilime.com/spinops/floodgate/cmd/gateclient"
 	"cl-gitlab.intra.codilime.com/spinops/floodgate/cmd/parser"
+	spr "cl-gitlab.intra.codilime.com/spinops/floodgate/cmd/spinnakerresource"
 )
 
 func main() {
@@ -14,5 +19,29 @@ func main() {
 
 	_ = p.LoadObjectsFromDirectories(floodgateConfig.Resources)
 	log.Print("resources: ", p.Resources)
+	client := gateclient.NewGateapiClient(floodgateConfig)
+	content, err := ioutil.ReadFile("/tmp/pipeline.json")
+	if err != nil {
+		fmt.Print(err)
+		os.Exit(1)
+	}
+	var pipeline spr.Resourcer
+	pipeline = spr.CreatePipeline("deploy-nginx", "1bfaa7c1-894c-4adb-9e51-c969bc38c984", "nginx", client, content)
+	needToSave, err := pipeline.IsChanged()
+	if err != nil {
+		fmt.Print(err)
+		os.Exit(1)
+	}
+	if needToSave {
+		fmt.Print("Saving local state to Spinnaker\n")
+		err := pipeline.SaveRemoteState(client)
+		if err != nil {
+			fmt.Print(err)
+			os.Exit(1)
+		}
+	} else {
+		fmt.Print("No need to save")
+	}
+	fmt.Printf("%T\n", pipeline)
 
 }
