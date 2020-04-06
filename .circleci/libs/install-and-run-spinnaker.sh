5405d884c5f91310bc5d4e7725d57633984d8d28
Dawid Trzebiatowski <boscode@boscard.pl>
2020-04-06 17:26:18 +0200

    Test pipeline with all supported versions

--- .circleci/libs/install-and-run-spinnaker.sh
+++ .circleci/libs/install-and-run-spinnaker.sh
@@ -6,11 +6,11 @@ hal -v
 kind create cluster
 kubectl config use-context kind-kind
 kubectl cluster-info
-hal config provider kubernetes enable
+hal -q config provider kubernetes enable
 CONTEXT=$(kubectl config current-context)
-hal config provider kubernetes account add my-k8s-v2-account --provider-version v2 --context $CONTEXT
-hal config features edit --artifacts true
-hal config deploy edit --type distributed --account-name my-k8s-v2-account
+hal -q config provider kubernetes account add my-k8s-v2-account --provider-version v2 --context $CONTEXT
+hal -q config features edit --artifacts true
+hal -q config deploy edit --type distributed --account-name my-k8s-v2-account
 kubectl create namespace spinnaker
 kubectl -n spinnaker create -f https://raw.githubusercontent.com/minio/minio/master/docs/orchestration/kubernetes/minio-standalone-pvc.yaml
 kubectl -n spinnaker create -f https://raw.githubusercontent.com/minio/minio/master/docs/orchestration/kubernetes/minio-standalone-deployment.yaml
@@ -21,11 +21,13 @@ mkdir -p ~/.hal/default/profiles
 echo "spinnaker.s3.versioning: false" >> ~/.hal/default/profiles/front50-local.yml
 export MINIO_ACCESS_KEY=minio
 export MINIO_SECRET_KEY=minio123
-echo $MINIO_SECRET_KEY | hal config storage s3 edit --path-style-access=true --endpoint "http://minio-service:9000" --access-key-id $MINIO_ACCESS_KEY --secret-access-key
-hal config storage edit --type s3
-hal version list
-hal config version edit --version 1.18.8
-hal deploy apply
+echo $MINIO_SECRET_KEY | hal -q config storage s3 edit --path-style-access=true --endpoint "http://minio-service:9000" --access-key-id $MINIO_ACCESS_KEY --secret-access-key
+hal -q config storage edit --type s3
+hal -q config features edit --pipeline-templates true
+NEED_SPINNAKER_VERSION=${NEED_SPINNAKER_VERSION:1.19}
+hal -q version list
+hal -q config version edit --version $(hal -q version list  | grep "^ -" | awk '{ print $2 }' | grep ${NEED_SPINNAKER_VERSION})
+hal -q deploy apply
 until kubectl -n spinnaker wait --for=condition=Ready pod --all > /dev/null
 do
 	kubectl -n spinnaker get pods
