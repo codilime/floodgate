1b65613062013f83995112e36f10329786948169
Dawid Trzebiatowski <boscard@users.noreply.github.com>
2020-04-10 16:34:35 +0200

    Testing ingress

--- .circleci/libs/install-and-run-spinnaker.sh
+++ .circleci/libs/install-and-run-spinnaker.sh
@@ -1,11 +1,35 @@
 #!/bin/bash -xe
+# Install Halyard
 curl -O https://raw.githubusercontent.com/spinnaker/halyard/master/install/debian/InstallHalyard.sh
 USERNAME=`whoami`
 sudo bash InstallHalyard.sh --user $USERNAME -y
 hal -v
-kind create cluster
+
+# Create Kind cluster
+cat <<EOF | kind create cluster --config=-
+kind: Cluster
+apiVersion: kind.x-k8s.io/v1alpha4
+nodes:
+- role: control-plane
+  kubeadmConfigPatches:
+  - |
+    kind: InitConfiguration
+    nodeRegistration:
+      kubeletExtraArgs:
+        node-labels: "ingress-ready=true"
+        authorization-mode: "AlwaysAllow"
+  extraPortMappings:
+  - containerPort: 80
+    hostPort: 80
+    protocol: TCP
+  - containerPort: 443
+    hostPort: 443
+    protocol: TCP
+EOF
 kubectl config use-context kind-kind
 kubectl cluster-info
+
+# Configure Spinnaker installation
 hal -q config provider kubernetes enable
 CONTEXT=$(kubectl config current-context)
 hal -q config provider kubernetes account add my-k8s-v2-account --provider-version v2 --context $CONTEXT
@@ -27,8 +51,38 @@ hal -q config features edit --pipeline-templates true
 export NEED_SPINNAKER_VERSION=${NEED_SPINNAKER_VERSION:-1.19}
 hal -q version list
 hal -q config version edit --version $(hal -q version list  | grep "^ -" | awk '{ print $2 }' | grep ${NEED_SPINNAKER_VERSION})
+
+# Install Spinnaker
 hal -q deploy apply
 until kubectl -n spinnaker wait --for=condition=Ready pod --all > /dev/null
 do
 	kubectl -n spinnaker get pods
 done
+
+# Install Ingress controller
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/nginx-0.30.0/deploy/static/mandatory.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/nginx-0.30.0/deploy/static/provider/baremetal/service-nodeport.yaml
+kubectl patch deployments -n ingress-nginx nginx-ingress-controller -p '{"spec":{"template":{"spec":{"containers":[{"name":"nginx-ingress-controller","ports":[{"containerPort":80,"hostPort":80},{"containerPort":443,"hostPort":443}]}],"nodeSelector":{"ingress-ready":"true"},"tolerations":[{"key":"node-role.kubernetes.io/master","operator":"Equal","effect":"NoSchedule"}]}}}}'
+cat << EOF > spin-ingress.yaml
+apiVersion: extensions/v1beta1
+kind: Ingress
+metadata:
+  labels:
+    app: spin
+  name: spin-ingress
+  namespace: spinnaker
+spec:
+  rules:
+  - http:
+      paths:
+      - backend:
+          serviceName: spin-deck
+          servicePort: 9000
+        path: /
+      - backend:
+          serviceName: spin-gate
+          servicePort: 8084
+        path: /api/v1
+EOF
+kubectl -n spinnaker apply -f spin-ingress.yaml
+
