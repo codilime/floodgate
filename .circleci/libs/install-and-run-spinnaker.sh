c1585fea55697ee306d8aee44669334dd707a36b
Dawid Trzebiatowski <boscard@users.noreply.github.com>
2020-04-14 23:34:53 +0200

    Updates for gate setup

--- .circleci/libs/install-and-run-spinnaker.sh
+++ .circleci/libs/install-and-run-spinnaker.sh
@@ -30,11 +30,15 @@ kubectl config use-context kind-kind
 kubectl cluster-info
 
 # Configure Spinnaker installation
+## Generate random password
+GATE_PASS=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 32 ; echo '')
+## Configure default provider
 hal -q config provider kubernetes enable
 CONTEXT=$(kubectl config current-context)
 hal -q config provider kubernetes account add my-k8s-v2-account --provider-version v2 --context $CONTEXT
 hal -q config features edit --artifacts true
 hal -q config deploy edit --type distributed --account-name my-k8s-v2-account
+## Prepare cluster
 kubectl create namespace spinnaker
 kubectl -n spinnaker create -f https://raw.githubusercontent.com/minio/minio/master/docs/orchestration/kubernetes/minio-standalone-pvc.yaml
 kubectl -n spinnaker create -f https://raw.githubusercontent.com/minio/minio/master/docs/orchestration/kubernetes/minio-standalone-deployment.yaml
@@ -43,6 +47,7 @@ sed -i 's/LoadBalancer/ClusterIP/g' minio-standalone-service.yaml
 kubectl -n spinnaker create -f minio-standalone-service.yaml
 mkdir -p ~/.hal/default/profiles
 echo "spinnaker.s3.versioning: false" >> ~/.hal/default/profiles/front50-local.yml
+## Configure Spinnaker
 export MINIO_ACCESS_KEY=minio
 export MINIO_SECRET_KEY=minio123
 echo $MINIO_SECRET_KEY | hal -q config storage s3 edit --path-style-access=true --endpoint "http://minio-service:9000" --access-key-id $MINIO_ACCESS_KEY --secret-access-key
@@ -53,6 +58,26 @@ hal -q version list
 hal -q config version edit --version $(hal -q version list  | grep "^ -" | awk '{ print $2 }' | grep ${NEED_SPINNAKER_VERSION})
 hal config security ui edit --override-base-url='http://spinnaker'
 hal config security api edit --override-base-url='http://spinnaker/api/v1'
+### Extra parameters for Gate and Deck
+echo 'window.spinnakerSettings.authEnabled = true;' > ~/.hal/default/profiles/settings-local.js
+echo 'healthEndpoint: /api/v1/health' > ~/.hal/default/service-settings/gate.yml
+cat <<EOF > ~/.hal/default/profiles/gate-local.yml
+server:
+  servlet:
+    context-path: /api/v1
+  tomcat:
+    protocolHeader: X-Forwarded-Proto
+    remoteIpHeader: X-Forwarded-For
+    internalProxies: .*
+    httpsServerPort: X-Forwarded-Port
+
+security:
+  basicform:
+    enabled: true
+  user:
+    name: admin
+    password: ${GATE_PASS}
+EOF
 
 # Install Spinnaker
 hal -q deploy apply
