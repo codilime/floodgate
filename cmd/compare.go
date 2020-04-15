841a9b96ef746b0940104f3bcfcad3781314eb9f
Marcin Wierzbicki <marcin.wierzb@gmail.com>
2020-04-15 19:38:04 +0200

    Add compare command (#51)
    * Implement compare command
    
    * Simplify output format
    
    * Edit output format

--- cmd/compare.go
+++ cmd/compare.go
@@ -1,9 +1,11 @@
 package cmd
 
 import (
+	"errors"
 	"fmt"
 	"io"
 
+	rm "github.com/codilime/floodgate/resourcemanager"
 	"github.com/spf13/cobra"
 )
 
@@ -26,5 +28,32 @@ func NewCompareCmd(out io.Writer) *cobra.Command {
 }
 
 func runCompare(cmd *cobra.Command, options compareOptions) error {
-	return fmt.Errorf("not implemented")
+	flags := cmd.InheritedFlags()
+	configPath, err := flags.GetString("config")
+	if err != nil {
+		return err
+	}
+	resourceManager := &rm.ResourceManager{}
+	if err := resourceManager.Init(configPath); err != nil {
+		return err
+	}
+	changes := resourceManager.GetChanges()
+	if len(changes) == 0 {
+		return nil
+	}
+	printCompareDiff(changes)
+	return errors.New("end diff")
+}
+
+func printCompareDiff(changes []rm.ResourceChange) {
+	for _, change := range changes {
+		var line string
+		if change.ID != "" {
+			line = fmt.Sprintf("%s (%s) (%s)", change.ID, change.Name, change.Type)
+		} else {
+			line = fmt.Sprintf("%s (%s)", change.Name, change.Type)
+		}
+		fmt.Println(line)
+		fmt.Println(change.Changes)
+	}
 }
