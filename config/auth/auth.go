e9ef755d408c4aa9bfc9f804c2ae413f6e76ebd0
Kacper Sawicki <kacper@ksawicki.pl>
2020-05-26 15:58:37 +0200

    Fix linter

--- config/auth/auth.go
+++ config/auth/auth.go
@@ -2,18 +2,21 @@ package auth
 
 import "golang.org/x/oauth2"
 
+// Config represents object for authentication
 type Config struct {
 	Basic  Basic  `json:"basic"`
 	OAuth2 OAuth2 `json:"oauth2"`
 	X509   X509   `json:"x509"`
 }
 
+// Basic represents object for basic authentication
 type Basic struct {
 	Enabled  bool   `json:"enabled"`
 	User     string `json:"user"`
 	Password string `json:"password"`
 }
 
+// OAuth2 represents object for oauth2 authentication
 type OAuth2 struct {
 	Enabled      bool         `json:"enabled"`
 	TokenURL     string       `json:"tokenUrl"`
@@ -24,13 +27,14 @@ type OAuth2 struct {
 	CachedToken  oauth2.Token `json:"cachedToken,omitempty"`
 }
 
+// X509 represents object for x509 authentication
 type X509 struct {
 	Enabled  bool   `json:"enabled"`
 	CertPath string `json:"certPath"`
 	KeyPath  string `json:"keyPath"`
 }
 
-// IsAuthValid is used to check if is only one auth method selected
+// IsValid is used to check if is only one auth method selected
 func (config *Config) IsValid() bool {
 	if config.Basic.Enabled && config.OAuth2.Enabled {
 		return false
