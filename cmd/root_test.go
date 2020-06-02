dde96c7d470707eebce2fff1afd9b40bb122c988
Kacper Sawicki <kacper@ksawicki.pl>
2020-06-02 16:14:07 +0200

    Test render cmd

--- cmd/root_test.go
+++ cmd/root_test.go
@@ -3,6 +3,8 @@ package cmd
 import (
 	"fmt"
 	"io/ioutil"
+	"path"
+	"runtime"
 )
 
 func CreateTempFiles(endpoint string) (string, string, error) {
@@ -29,6 +31,12 @@ func CreateTempFiles(endpoint string) (string, string, error) {
 		return "", "", err
 	}
 
+	pipelineJsonnet, err := ioutil.TempFile(pipelinesDir, "*.jsonnetpipeline.jsonnet")
+	if err != nil {
+		return "", "", err
+	}
+	ioutil.WriteFile(pipelineJsonnet.Name(), []byte("local pipelines = import 'pipeline.libsonnet';\n\npipelines.pipeline()\n.withName('Example pipeline from Jsonnet')\n.withId('jsonnetpipeline')\n.withApplication('jsonnetapp')"), 0644)
+
 	pipeline, err := ioutil.TempFile(pipelinesDir, "*.jsonpipeline.json")
 	if err != nil {
 		return "", "", err
@@ -47,12 +55,16 @@ func CreateTempFiles(endpoint string) (string, string, error) {
 	}
 	ioutil.WriteFile(pipelineTemplate.Name(), []byte("{\n   \"id\": \"jsonnetpt\",\n   \"metadata\": {\n      \"description\": \"Example pipeline template created from Jsonnet file.\",\n      \"name\": \"Example pipeline template from Jsonnet\",\n      \"owner\": \"floodgate@example.com\",\n      \"scopes\": [\n         \"global\"\n      ]\n   },\n   \"protect\": false,\n   \"schema\": \"v2\",\n   \"variables\": [ ]\n}"), 0644)
 
+	//Get sponnet directory
+	_, filename, _, _ := runtime.Caller(0)
+	sponnetDir := path.Join(path.Dir(filename), "../sponnet")
+
 	//Create config file
 	config, err := ioutil.TempFile(dir, "*.config.yaml")
 	if err != nil {
 		return "", "", err
 	}
-	configStr := fmt.Sprintf("endpoint: %s\ninsecure: true\nauth:\n  basic:\n    enabled: true\n    user: admin\n    password: VRCm9L80yO3FHTKeVthtxknfGq1b10WqInKoBFqozphGcrGi\nlibraries:\n  - sponnet\nresources:\n  - %s\n  - %s\n  - %s", endpoint, applicationsDir, pipelinesDir, pipelineTemplatesDir)
+	configStr := fmt.Sprintf("endpoint: %s\ninsecure: true\nauth:\n  basic:\n    enabled: true\n    user: admin\n    password: VRCm9L80yO3FHTKeVthtxknfGq1b10WqInKoBFqozphGcrGi\nlibraries:\n  - %s\nresources:\n  - %s\n  - %s\n  - %s", endpoint, sponnetDir, applicationsDir, pipelinesDir, pipelineTemplatesDir)
 	ioutil.WriteFile(config.Name(), []byte(configStr), 0644)
 
 	return dir, config.Name(), nil
