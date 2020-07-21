72df636b2c442adc5769045b4ab41474d1ec3e0e
Rafał Paradowski <contact@rafalparadowski.com>
2020-07-21 13:54:06 +0200

    added missing descriptions of executed commands

--- .cilibs/test_floodgate_against_running_spinnaker_instance.sh
+++ .cilibs/test_floodgate_against_running_spinnaker_instance.sh
@@ -3,9 +3,15 @@
 FLOODGATE_EXTRA_PARAMS=$1
 
 echo Test Floodgate against running Spinnaker instance
+
+echo "Print version using version flag"
 /floodgate/bin/floodgate --version
+echo "Print version using version command"
 /floodgate/bin/floodgate version
+echo "Comare changes - clean Spinnaker"
 /floodgate/bin/floodgate $FLOODGATE_EXTRA_PARAMS --config ~/floodgate.yaml compare && exit 1 || echo "Found changes"
+echo "Apply local resources"
 /floodgate/bin/floodgate $FLOODGATE_EXTRA_PARAMS --config ~/floodgate.yaml sync
+echo "Compare changes - synced resources"
 /floodgate/bin/floodgate $FLOODGATE_EXTRA_PARAMS --config ~/floodgate.yaml compare
 
