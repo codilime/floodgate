509705413e0a52311f8f316c561a62e421dc780c
Kacper Sawicki <kacper@ksawicki.pl>
2020-07-30 14:43:21 +0200

    Trigger webhook through cmd

--- cmd/execute.go
+++ cmd/execute.go
@@ -1,6 +1,10 @@
 package cmd
 
 import (
+	c "github.com/codilime/floodgate/config"
+	swagger "github.com/codilime/floodgate/gateapi"
+	rm "github.com/codilime/floodgate/resourcemanager"
+	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 	"io"
 )
@@ -27,5 +31,38 @@ func NewExecuteCmd(out io.Writer) *cobra.Command {
 }
 
 func runExecute(cmd *cobra.Command, options executeOptions) error {
+	flags := cmd.InheritedFlags()
+	configPath, err := flags.GetString("config")
+	if err != nil {
+		return err
+	}
+	config, err := c.LoadConfig(configPath)
+	if err != nil {
+		return err
+	}
+	config.Merge(cfg)
+
+	resourceManager := &rm.ResourceManager{}
+	if err := resourceManager.Init(config); err != nil {
+		return err
+	}
+	client := resourceManager.GetClient()
+
+	log.Infof("triggering '%s'", options.webhookName)
+
+	var opts *swagger.WebhooksUsingPOSTOpts
+	payload, resp, err := client.WebhookControllerApi.WebhooksUsingPOST(client.Context, options.webhookName, "webhook", opts)
+	if err != nil {
+		return err
+	}
+
+	if resp != nil {
+		data := payload.(map[string]interface{})
+		if data["eventProcessed"].(bool) {
+			log.Info("event processed successfully")
+			log.Infof("execution id is %s", data["eventId"])
+		}
+	}
+
 	return nil
 }
