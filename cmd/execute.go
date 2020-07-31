8d99f3ee23c203da79374dce7b6929f3c2a5d3be
Kacper Sawicki <kacper@ksawicki.pl>
2020-07-31 13:10:37 +0200

    Wait for pipeline execution to finish

--- cmd/execute.go
+++ cmd/execute.go
@@ -1,16 +1,22 @@
 package cmd
 
 import (
+	"errors"
+	"github.com/antihax/optional"
 	c "github.com/codilime/floodgate/config"
 	swagger "github.com/codilime/floodgate/gateapi"
+	gc "github.com/codilime/floodgate/gateclient"
 	rm "github.com/codilime/floodgate/resourcemanager"
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 	"io"
+	"time"
 )
 
 type executeOptions struct {
 	webhookName string
+	wait        bool
+	waitTime    int
 }
 
 func NewExecuteCmd(out io.Writer) *cobra.Command {
@@ -26,6 +32,9 @@ func NewExecuteCmd(out io.Writer) *cobra.Command {
 		},
 		Args: cobra.MinimumNArgs(1),
 	}
+
+	cmd.Flags().BoolVarP(&options.wait, "wait", "w", false, "wait for pipeline execution to finish")
+	cmd.Flags().IntVarP(&options.waitTime, "wait-time", "t", 30, "wait time between status check")
 
 	return cmd
 }
@@ -51,18 +60,68 @@ func runExecute(cmd *cobra.Command, options executeOptions) error {
 	log.Infof("triggering '%s'", options.webhookName)
 
 	var opts *swagger.WebhooksUsingPOSTOpts
-	payload, resp, err := client.WebhookControllerApi.WebhooksUsingPOST(client.Context, options.webhookName, "webhook", opts)
+	payload, resp, err := client.WebhookControllerApi.WebhooksUsingPOST(client.Context, options.webhookName,
+		"webhook", opts)
 	if err != nil {
 		return err
 	}
 
-	if resp != nil {
-		data := payload.(map[string]interface{})
-		if data["eventProcessed"].(bool) {
-			log.Info("event processed successfully")
-			log.Infof("execution id is %s", data["eventId"])
+	if resp == nil {
+		return errors.New("spinnaker API response is empty")
+	}
+	data := payload.(map[string]interface{})
+	if !data["eventProcessed"].(bool) {
+		return errors.New("event not processed")
+	}
+
+	log.Info("event processed successfully")
+	log.Infof("execution id is %s", data["eventId"])
+
+	for options.wait {
+		status, err := executionStatus(client, data["eventId"].(string))
+		if err != nil {
+			return err
 		}
+
+		switch status {
+		case "NOT_STARTED":
+			log.Info("waiting for pipeline to start")
+		case "RUNNING":
+			log.Info("pipeline is still running")
+		case "SUCCEEDED":
+			log.Info("pipeline succeeded")
+		default:
+			log.Info("something went wrong")
+		}
+
+		if status == "SUCCEEDED" {
+			break
+		}
+
+		time.Sleep(time.Duration(options.waitTime) * time.Second)
 	}
 
 	return nil
+}
+
+func executionStatus(spinnakerAPI *gc.GateapiClient, eventId string) (string, error) {
+	var opts swagger.SearchForPipelineExecutionsByTriggerUsingGETOpts
+	opts.TriggerTypes = optional.NewString("webhook")
+	opts.EventId = optional.NewString(eventId)
+
+	payload, resp, err := spinnakerAPI.ExecutionsControllerApi.SearchForPipelineExecutionsByTriggerUsingGET(spinnakerAPI.Context,
+		"*", &opts)
+	if err != nil {
+		return "", err
+	}
+
+	if resp != nil {
+		data := payload[0].(map[string]interface{})
+
+		if status, ok := data["status"].(string); ok {
+			return status, nil
+		}
+	}
+
+	return "", nil
 }
