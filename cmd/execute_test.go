d85021bb89808ac5511c3d6739b4b737dbd9fd08
Kacper Sawicki <kacper@ksawicki.pl>
2020-07-31 14:37:47 +0200

    Test execute wait

--- cmd/execute_test.go
+++ cmd/execute_test.go
@@ -47,8 +47,50 @@ func TestExecute(t *testing.T) {
 
 	outStr := strings.TrimSpace(string(out))
 	if outStr != executeWant {
-		t.Errorf("cmd.Execute() got:\n %s want:\n %s", outStr, inspectWant)
+		t.Errorf("cmd.Execute() got:\n %s want:\n %s", outStr, executeWant)
+	}
+}
+
+func TestExecuteWait(t *testing.T) {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/auth/loggedOut", test.MockGateServerAuthLoggedOutHandler)
+	mux.HandleFunc("/webhooks/webhook/test", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.Write([]byte("{\"eventId\": \"123\",\"eventProcessed\": true}"))
+	})
+	mux.HandleFunc("/applications/*/executions/search", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.Write([]byte("[{\n  \"status\": \"SUCCEEDED\"}]"))
+	})
+	ts := httptest.NewServer(mux)
+
+	dir, config, err := CreateTempFiles(ts.URL, false)
+	if err != nil {
+		t.Errorf("cmd.Execute() Error while creating temp config %v", err)
+	}
+	defer RemoveTempDir(dir)
+
+	b := bytes.NewBufferString("")
+	cmd := NewRootCmd(b)
+	cmd.SetOut(b)
+
+	cmd.SetArgs([]string{"--config=" + config, "execute", "test", "-w", "--wait-time=1"})
+
+	err = cmd.Execute()
+	if err != nil {
+		t.Fatalf("cmd.Execute() Execute err: %v", err)
+	}
+
+	out, err := ioutil.ReadAll(b)
+	if err != nil {
+		t.Fatalf("cmd.Execute() Read output err: %v", err)
+	}
+
+	outStr := strings.TrimSpace(string(out))
+	if outStr != executeWaitWant {
+		t.Errorf("cmd.Execute() got:\n %s want:\n %s", outStr, executeWant)
 	}
 }
 
 var executeWant = "triggering 'test'\nevent processed successfully\nexecution id is 123"
+var executeWaitWant = "triggering 'test'\nevent processed successfully\nexecution id is 123\npipeline succeeded"
